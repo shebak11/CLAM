6741b8956b8723d7cfe9410098d4464fb453b7a5
shebak11 <sbakr@stanford.edu>
2023-11-04 14:52:51 -0700

    Update extract_features_fp_xla.py

--- extract_features_fp_xla.py
+++ extract_features_fp_xla.py
@@ -122,7 +122,8 @@ if __name__ == '__main__':
 		#storage_client = storage.Client()
 		path = args.data_slide_dir+slide_id+args.slide_ext    
 		blob = bucket.blob(path)
-		slide_file_path = "/home/MacOS/"+ slide_id+args.slide_ext    
+        print(os.path.basename(slide_id))
+		slide_file_path = "/home/MacOS/"+ os.path.basename(slide_id)+args.slide_ext    
 		blob.download_to_filename(slide_file_path )
 		#self.wsi = openslide.OpenSlide(path) 
 		wsi = openslide.open_slide(slide_file_path)
