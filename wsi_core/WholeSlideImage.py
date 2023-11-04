1ba42eee247aab21e1c9aec2a49a48de21918bac
shebak11 <sbakr@stanford.edu>
2023-11-04 10:38:46 -0700

    Update WholeSlideImage.py

--- wsi_core/WholeSlideImage.py
+++ wsi_core/WholeSlideImage.py
@@ -17,6 +17,7 @@ import itertools
 from wsi_core.util_classes import isInContourV1, isInContourV2, isInContourV3_Easy, isInContourV3_Hard, Contour_Checking_fn
 from utils.file_utils import load_pkl, save_pkl
 import tiffslide as openslide
+import gcsfs
 
 Image.MAX_IMAGE_PIXELS = 933120000
 
