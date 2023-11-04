9312c9e125174c06850725bdad5efe23932bbdc3
shebak11 <sbakr@stanford.edu>
2023-11-04 15:33:07 -0700

    Update dataset_h5.py

--- datasets/dataset_h5.py
+++ datasets/dataset_h5.py
@@ -63,7 +63,7 @@ class Whole_Slide_Bag(Dataset):
 		#self.file_path = file_path
 		file_name =  os.path.splitext(os.path.basename(file_path))[0]
 		print("h5 file " + file_name)
-		local_file_path = "/home/MacOS/"+ self.name+ '.svs'
+		local_file_path = "/home/MacOS/"+ file_name+ '.h5'
 		self.file_path = local_file_path
 		storage_client = storage.Client()
 		bucket = storage_client.bucket("oncomerge")
