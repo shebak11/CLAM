236771422de1ff19a7ec74b93b2832c1dd237358
shebak11 <sbakr@stanford.edu>
2023-11-04 15:34:53 -0700

    Update dataset_h5.py

--- datasets/dataset_h5.py
+++ datasets/dataset_h5.py
@@ -60,16 +60,8 @@ class Whole_Slide_Bag(Dataset):
 		else:
 			self.roi_transforms = custom_transforms
 
-		#self.file_path = file_path
-		file_name =  os.path.splitext(os.path.basename(file_path))[0]
-		print("h5 file " + file_name)
-		local_file_path = "/home/MacOS/"+ file_name+ '.h5'
-		self.file_path = local_file_path
-		storage_client = storage.Client()
-		bucket = storage_client.bucket("oncomerge")
-		gs_path = file_path
-		blob = bucket.blob(gs_path)
-		blob.download_to_filename(self.file_path )
+		self.file_path = file_path
+		
 
 		with h5py.File(self.file_path, "r") as f:
 			dset = f['imgs']
@@ -128,7 +120,7 @@ class Whole_Slide_Bag_FP(Dataset):
 		#self.file_path = file_path
 		file_name =  os.path.splitext(os.path.basename(file_path))[0]
 		print("h5 file " + file_name)
-		local_file_path = "/home/MacOS/"+ self.name+ '.svs'
+		local_file_path = "/home/MacOS/"+ file_name+ '.h5'
 		self.file_path = local_file_path
 		storage_client = storage.Client()
 		bucket = storage_client.bucket("oncomerge")
