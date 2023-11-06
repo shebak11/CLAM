88b4f7c453ccbcdb5565cdc1faa9f5595ff19e41
shebak11 <sbakr@stanford.edu>
2023-11-06 15:37:32 -0800

    Update test_train_mp_imagenet.py

--- test_train_mp_imagenet.py
+++ test_train_mp_imagenet.py
@@ -403,6 +403,12 @@ def _mp_fn(index, flags):
   global FLAGS
   FLAGS = flags
   torch.set_default_dtype(torch.float32)
+  local_file_path = "/home/MacOS/TCGA-3L-AA1B-01A-01-TS1.9C415218-D5B4-4945-B243-F42A4C8C0484.h5"
+  with h5py.File(local_file_path, "r") as f:
+    dset = f['coords']
+    x = f['coords'].attrs['patch_level']
+    y = f['coords'].attrs['patch_size']
+    z = len(dset)
 
   accuracy = train_imagenet()
   if accuracy < FLAGS.target_accuracy:
