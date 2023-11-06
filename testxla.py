a64eea09d19af3d965425f5b600a79f33114658a
shebak11 <sbakr@stanford.edu>
2023-11-05 22:01:15 -0800

    Update testxla.py

--- testxla.py
+++ testxla.py
@@ -143,7 +143,10 @@ def train_imagenet():
     
     if FLAGS.ddp or FLAGS.pjrt_distributed:
         dist.init_process_group('xla', init_method='xla://')
-    print("FLAGS.ddp "  + FLAGS.ddp  + "FLAGS.pjrt_distributed" +FLAGS.pjrt_distributed )
+    print("FLAGS.ddp "  )
+    print(FLAGS.ddp  + 
+    print("FLAGS.pjrt_distributed" )
+    print(FLAGS.pjrt_distributed )
 
     print('==> Preparing data..')
     img_dim = get_model_property('img_dim')
