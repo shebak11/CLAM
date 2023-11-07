821112171e84e0f26c2229292d7c9a8b82a10433
shebak11 <sbakr@stanford.edu>
2023-11-06 20:34:36 -0800

    Update dataset_h5.py

--- datasets/dataset_h5.py
+++ datasets/dataset_h5.py
@@ -129,19 +129,7 @@ class Whole_Slide_Bag_FP(Dataset):
         
 		blob = bucket.blob(gs_path)
 		blob.download_to_filename(self.file_path )
-"""
-		with h5py.File(self.file_path, "r") as f:
-			dset = f['coords']
-			self.patch_level = f['coords'].attrs['patch_level']
-			self.patch_size = f['coords'].attrs['patch_size']
-			self.length = len(dset)
-			if target_patch_size > 0:
-				self.target_patch_size = (target_patch_size, ) * 2
-			elif custom_downsample > 1:
-				self.target_patch_size = (self.patch_size // custom_downsample, ) * 2
-			else:
-				self.target_patch_size = None
-"""
+
 
 		self.summary()
 			
