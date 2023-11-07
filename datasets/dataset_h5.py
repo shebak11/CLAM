92da50692a8225c1d228310294f727bea35d96a7
shebak11 <sbakr@stanford.edu>
2023-11-07 15:01:08 -0800

    Update dataset_h5.py

--- datasets/dataset_h5.py
+++ datasets/dataset_h5.py
@@ -131,7 +131,7 @@ class Whole_Slide_Bag_FP(Dataset):
 		blob.download_to_filename(self.file_path )
 
 		with h5py.File(self.file_path, "r") as f:
-			dset = f['coords']
+			#dset = f['coords']
 			#self.patch_level = f['coords'].attrs['patch_level']
 			#self.patch_size = f['coords'].attrs['patch_size']
 			#self.length = len(dset)
