47fb4b720019aff22d450734c5c1b4ea4da70ce9
shebak11 <sbakr@stanford.edu>
2023-11-07 21:09:04 -0800

    Update test_train_mp_imagenet.py

--- test_train_mp_imagenet.py
+++ test_train_mp_imagenet.py
@@ -331,11 +331,12 @@ def train_imagenet():
   loader = DataLoader( dataset,
         batch_size=FLAGS.batch_size,
         sampler=train_sampler,
-        drop_last=FLAGS.drop_last,
-        shuffle=False if train_sampler else True,
+        #drop_last=FLAGS.drop_last,
+        #shuffle=False if train_sampler else True,
         num_workers=FLAGS.num_workers,
-        persistent_workers=FLAGS.persistent_workers,
-        prefetch_factor=FLAGS.prefetch_factor, collate_fn=collate_features)
+        #persistent_workers=FLAGS.persistent_workers,
+        #prefetch_factor=FLAGS.prefetch_factor, 
+        collate_fn=collate_features)
 
   model = get_model_property('model_fn')().to(device)
   #model = resnet50_baseline(pretrained=True)
