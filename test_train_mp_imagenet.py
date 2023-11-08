7b255a624be83f408737d77702b0012ed6c4434a
shebak11 <sbakr@stanford.edu>
2023-11-07 20:48:14 -0800

    Update test_train_mp_imagenet.py

--- test_train_mp_imagenet.py
+++ test_train_mp_imagenet.py
@@ -323,6 +323,8 @@ def train_imagenet():
   torch.manual_seed(42)
   device = xm.xla_device()
   dataset = Whole_Slide_Bag_FP(file_path=file_path, wsi=wsi, pretrained=pretrained,  custom_downsample=custom_downsample, target_patch_size=target_patch_size)
+  train_sampler, test_sampler = None, None
+
   x, y = dataset[0]  
   #kwargs = {'num_workers': 4, 'pin_memory': True} if device.type == "cuda" else {}
 
