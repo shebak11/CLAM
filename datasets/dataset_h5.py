7c3cc5cd0cff1261eea4918f2c55bc8dbea26b9e
shebak11 <sbakr@stanford.edu>
2023-11-09 12:27:29 -0800

    Update dataset_h5.py

--- datasets/dataset_h5.py
+++ datasets/dataset_h5.py
@@ -175,7 +175,7 @@ class Whole_Slide_Bag_FP(Dataset):
 		#img = self.wsi.read_region(coord, self.patch_level, (self.patch_size, self.patch_size)).convert('RGB')
 		#img = self.img
 		img = self.wsi.read_region(self.coord, level = self.patch_level, size = (self.patch_size, self.patch_size)).convert('RGB')
-		#img = self.wsi.read_region(self.coord, self.patch_level, (self.patch_size, self.patch_size))
+		img = self.wsi.read_region((300, 400), level = 0, size = (512, 512)).convert('RGB')
 
 
 		if self.target_patch_size is not None:
