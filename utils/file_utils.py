2b1ee70cd1e53909bcf1d18e54ada60bf807c5f0
shebak11 <sbakr@stanford.edu>
2023-11-09 21:22:01 -0800

    Update file_utils.py

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -15,6 +15,7 @@ def load_pkl(filename):
 
 def save_hdf5(output_path, asset_dict, attr_dict= None, mode='a'):
     file = h5py.File(output_path, mode)
+    """
     for key, val in asset_dict.items():
         data_shape = val.shape
         if key not in file:
@@ -31,5 +32,6 @@ def save_hdf5(output_path, asset_dict, attr_dict= None, mode='a'):
             dset = file[key]
             dset.resize(len(dset) + data_shape[0], axis=0)
             dset[-data_shape[0]:] = val
+    """
     file.close()
     return output_path
