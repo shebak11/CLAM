266d2b30c821f60b975e20af4b46fc57596b9b89
shebak11 <sbakr@stanford.edu>
2023-11-09 19:57:17 -0800

    Update test_train_mp_imagenet.py

--- test_train_mp_imagenet.py
+++ test_train_mp_imagenet.py
@@ -479,17 +479,11 @@ def train_imagenet():
         asset_dict = {'features': features, 'coords': coords}
         save_hdf5(local_output_path, asset_dict, attr_dict= None, mode=mode)
         mode = 'a'
-
-
-  #
-
-
-  name = 'file_i_want_to_check.txt'   
+  
   storage_client = storage.Client()
-
   bucket = storage_client.bucket("oncomerge")
   stats = storage.Blob(bucket=bucket, name=output_path).exists(storage_client)
-  print(type(Stats))
+  print(type(stats))
   print(stats)
   print("nnnnnnnnnnnn")
   
