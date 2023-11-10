cc2b174451bd2edbecd6d063746b924078de56bc
shebak11 <sbakr@stanford.edu>
2023-11-10 09:40:51 -0800

    Update test_train_mp_imagenet.py

--- test_train_mp_imagenet.py
+++ test_train_mp_imagenet.py
@@ -339,7 +339,7 @@ def train_imagenet(index =0):
   device = xm.xla_device()
   dataset = Whole_Slide_Bag_FP(file_path=file_path, wsi=wsi, pretrained=pretrained,  custom_downsample=custom_downsample, target_patch_size=target_patch_size)
   train_sampler, test_sampler = None, None
-  quit()
+  #quit()
   k = dataset[0]  
   file = open('data.pkl', 'wb')
   #Pickle dictionary using protocol 0.
