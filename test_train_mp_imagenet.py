8e4f64c3e600bbaa7b2e855b78211bea3ea9c7f7
shebak11 <sbakr@stanford.edu>
2023-11-13 15:08:10 -0800

    Update test_train_mp_imagenet.py

--- test_train_mp_imagenet.py
+++ test_train_mp_imagenet.py
@@ -299,8 +299,8 @@ def train_imagenet(index =0):
         print(coord.shape)
         print(type(coord[0]))
     img = wsi.read_region((coord[0], coord[1]), level= 0, size = (512, 512)).convert('RGB')   
-    #os.remove( "/home/MacOS/"+ bag_name)
-    #os.remove( "/home/MacOS/"+ file_id+ slide_ext)
+    os.remove( "/home/MacOS/"+ bag_name)
+    os.remove( "/home/MacOS/"+ file_id+ slide_ext)
 
   quit()
   #print((np.array([coord]).shape))
