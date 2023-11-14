0ae0fba0dba02726bc5b60d77ce092a3e2f6cbec
shebak11 <sbakr@stanford.edu>
2023-11-14 04:52:27 -0800

    Update test_train_mp_imagenet.py

--- test_train_mp_imagenet.py
+++ test_train_mp_imagenet.py
@@ -373,13 +373,13 @@ def train_imagenet(index =0):
 
   torch.manual_seed(42)
   device = xm.xla_device()
-  for i in range(2):
+  for bag_candidate_idx in range(2):
     slide_id = bags_dataset[bag_candidate_idx].split(slide_ext)[0]
     file_id = os.path.basename(slide_id)
     bag_name = os.path.basename(slide_id)+'.h5'
     gs_file_path = os.path.join(data_h5_dir, bag_name)
     print("gs_file_path: " +gs_file_path)
-    dataset = Whole_Slide_Bag_FP(file_path=local_file_path_arr[i], wsi=wsi, pretrained=pretrained,  custom_downsample=custom_downsample, target_patch_size=target_patch_size)
+    dataset = Whole_Slide_Bag_FP(file_path=local_file_path_arr[bag_candidate_idx], wsi=wsi, pretrained=pretrained,  custom_downsample=custom_downsample, target_patch_size=target_patch_size)
   train_sampler, test_sampler = None, None
   #quit()
   k = dataset[0]  
