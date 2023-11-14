97bd565d61e6e319a19509f021e4d705cc1a27f3
shebak11 <sbakr@stanford.edu>
2023-11-14 04:40:39 -0800

    Update test_train_mp_imagenet.py

--- test_train_mp_imagenet.py
+++ test_train_mp_imagenet.py
@@ -347,7 +347,7 @@ def train_imagenet(index =0):
   """
   torch.manual_seed(42)
   device = xm.xla_device()
-  for i in range(3):
+  for i in range(2):
   
     dataset = Whole_Slide_Bag_FP(file_path=local_file_path_arr[i], wsi=wsi, pretrained=pretrained,  custom_downsample=custom_downsample, target_patch_size=target_patch_size)
   train_sampler, test_sampler = None, None
