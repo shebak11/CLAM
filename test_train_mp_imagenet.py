8f13047f1ad0bb0f5f9749fba17df420d22a494d
shebak11 <sbakr@stanford.edu>
2023-11-13 18:26:34 -0800

    Update test_train_mp_imagenet.py

--- test_train_mp_imagenet.py
+++ test_train_mp_imagenet.py
@@ -631,20 +631,7 @@ def train_imagenet(index =0):
     print("data to model")
     print(len(batch))
     print(batch.shape)
-   """
-    if count==50:
-      break
-    with torch.no_grad():	
-    #with torch.no_grad():	
-        if count % print_every == 20:
-            print('batch {}/{}, {} files processed'.format(count, len(loader), count * batch_size))
-        #batch = batch.to(device, non_blocking=True)
-        #features = model(batch) 
-        #features = features.cpu().numpy()
-        #asset_dict = {'features': features, 'coords': coords}
-        #save_hdf5(local_output_path, asset_dict, attr_dict= None, mode=mode)
-        mode = 'a'
-   """
+
   #storage_client = storage.Client()
   #bucket = storage_client.bucket("oncomerge")
   #stats = storage.Blob(bucket=bucket, name=output_path).exists(storage_client)
