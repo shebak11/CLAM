3f4917bced0228af1c2d37b461f3864c38ea38c8
shebak11 <sbakr@stanford.edu>
2023-11-14 06:32:11 -0800

    Update test_train_mp_imagenet.py

--- test_train_mp_imagenet.py
+++ test_train_mp_imagenet.py
@@ -370,15 +370,15 @@ def train_imagenet(index =0):
   local_file_path = "/home/MacOS/"+"TCGA-3L-AA1B-01A-01-TS1.9C415218-D5B4-4945-B243-F42A4C8C0484.h5"
 
 
-  #slide_id = bags_dataset[bag_candidate_idx].split(slide_ext)[0]
-  #file_id = os.path.basename(slide_id)
+  slide_id = bags_dataset[bag_candidate_idx].split(slide_ext)[0]
+  file_id = os.path.basename(slide_id)
   #bag_name = os.path.basename(slide_id)+'.h5'
   #gs_file_path = os.path.join(data_h5_dir, bag_name)
-  gs_slide_file_path = os.path.join(data_slide_dir, file_id+slide_ext)
+  #gs_slide_file_path = os.path.join(data_slide_dir, file_id+slide_ext)
   #local_slide_file_path = "/home/MacOS/"+ file_id+slide_ext
   #local_file_path = "/home/MacOS/"+bag_name
-  blob = bucket.blob(gs_slide_file_path)
-  blob.download_to_filename(local_slide_file_path )
+  #blob = bucket.blob(gs_slide_file_path)
+  #blob.download_to_filename(local_slide_file_path )
   wsi =     TiffSlide(slide_file_path)
   dataset = Whole_Slide_Bag_FP(file_path=file_path, wsi=wsi, pretrained=pretrained,  custom_downsample=custom_downsample, target_patch_size=target_patch_size)
   train_sampler, test_sampler = None, None
