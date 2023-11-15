c32ac72e0ab7ac926f34b0c9203c35b7b08ad7c9
shebak11 <sbakr@stanford.edu>
2023-11-15 12:36:53 -0800

    Update test_train_mp_imagenetO.py

--- test_train_mp_imagenetO.py
+++ test_train_mp_imagenetO.py
@@ -252,9 +252,9 @@ def train_imagenet(index=0):
       img_dim = get_model_property('img_dim')
 
       if FLAGS.fake_data:
-        print(FLAGS.num_workers)
-        print(FLAGS.persistent_workers)
-        print(FLAGS.prefetch_factor)
+        print(FLAGS.num_workers) #4
+        print(FLAGS.persistent_workers) #false
+        print(FLAGS.prefetch_factor) #16
         #quit()
         train_dataset_len = 1200000  # Roughly the size of Imagenet dataset.
         train_loader = xu.SampleGenerator(
@@ -272,7 +272,7 @@ def train_imagenet(index=0):
         print((dataset[0][1]))
         test_sampler = None, None
         
-        my_test_loader = torch.utils.data.DataLoader(
+        test_loader = torch.utils.data.DataLoader(
             dataset,
             batch_size=FLAGS.test_set_batch_size,
             ##sampler=test_sampler,
@@ -448,7 +448,7 @@ def train_imagenet(index=0):
       temp = next(iter(test_loader)) 
       print(len(temp))
       print(FLAGS.num_workers)
-      #quit()
+      quit()
       for epoch in range(1, FLAGS.num_epochs + 1):
         xm.master_print('Epoch {} train begin {}'.format(epoch, test_utils.now()))
         #train_loop_fn(train_device_loader, epoch)
