e5bc5c6e29b73f96cac7aac78bc70ce26ac3f975
shebak11 <sbakr@stanford.edu>
2023-11-15 12:35:01 -0800

    Update test_train_mp_imagenetO.py

--- test_train_mp_imagenetO.py
+++ test_train_mp_imagenetO.py
@@ -255,7 +255,7 @@ def train_imagenet(index=0):
         print(FLAGS.num_workers)
         print(FLAGS.persistent_workers)
         print(FLAGS.prefetch_factor)
-        quit()
+        #quit()
         train_dataset_len = 1200000  # Roughly the size of Imagenet dataset.
         train_loader = xu.SampleGenerator(
             data=(torch.zeros(FLAGS.batch_size, 3, img_dim, img_dim),
@@ -448,7 +448,7 @@ def train_imagenet(index=0):
       temp = next(iter(test_loader)) 
       print(len(temp))
       print(FLAGS.num_workers)
-      quit()
+      #quit()
       for epoch in range(1, FLAGS.num_epochs + 1):
         xm.master_print('Epoch {} train begin {}'.format(epoch, test_utils.now()))
         #train_loop_fn(train_device_loader, epoch)
