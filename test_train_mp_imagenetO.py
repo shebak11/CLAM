3d47f5bd801ea4cfcc30feabe0078d69ffd0c90e
shebak11 <sbakr@stanford.edu>
2023-11-15 15:31:15 -0800

    Update test_train_mp_imagenetO.py

--- test_train_mp_imagenetO.py
+++ test_train_mp_imagenetO.py
@@ -268,7 +268,7 @@ def train_imagenet(index=0):
             sample_count=50000 // FLAGS.batch_size // xm.xrt_world_size())
         dataset = Whole_Slide_Bag_FP(file_path=gs_file_path, gs_slide_file_path=gs_slide_file_path, pretrained=pretrained,  custom_downsample=custom_downsample, target_patch_size=target_patch_size)
         #dataset=dataset[0:1000][:]
-        dataset = torch.utils.data.Subset(dataset, [i for i in range(1000)])
+        #dataset = torch.utils.data.Subset(dataset, [i for i in range(1000)])
         #print(len(dataset))
         #print(np.array(dataset[0][0]).shape)
         #print((dataset[0][1]))
@@ -447,7 +447,7 @@ def train_imagenet(index=0):
           device_prefetch_size=FLAGS.device_prefetch_size,
           host_to_device_transfer_threads=FLAGS.host_to_device_transfer_threads)
       accuracy, max_accuracy = 0.0, 0.0
-      #print("test_loader ",str(len(test_loader)))
+      print("test_loader ",str(len(test_loader)))
       #print(xm.xrt_world_size())
       #print((FLAGS.test_set_batch_size))
       #print(len(test_device_loader))
