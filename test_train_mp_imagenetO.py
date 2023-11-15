eed8f1fc16ed810b90adfa1c8fc38b267118b799
shebak11 <sbakr@stanford.edu>
2023-11-15 14:27:44 -0800

    Update test_train_mp_imagenetO.py

--- test_train_mp_imagenetO.py
+++ test_train_mp_imagenetO.py
@@ -452,7 +452,7 @@ def train_imagenet(index=0):
       print(len(temp))
       print(FLAGS.num_workers)
       print(temp[0].shape)
-      quit()
+      #quit()
       for epoch in range(1, FLAGS.num_epochs + 1):
         xm.master_print('Epoch {} train begin {}'.format(epoch, test_utils.now()))
         #train_loop_fn(train_device_loader, epoch)
