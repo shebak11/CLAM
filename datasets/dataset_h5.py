d58700be984c12df9b56cad93752bb164f791d43
shebak11 <sbakr@stanford.edu>
2023-11-15 16:12:57 -0800

    Update dataset_h5.py

--- datasets/dataset_h5.py
+++ datasets/dataset_h5.py
@@ -198,7 +198,7 @@ class Whole_Slide_Bag_FP(Dataset):
 		storage_client = storage.Client()
 		bucket = storage_client.bucket("oncomerge")
 		
-        blob = bucket.blob(self.file_path)
+		blob = bucket.blob(self.file_path)
 		with blob.open("rb") as f:
 		  with h5py.File(f,'r') as hdf5_file:
 		      coord = hdf5_file['coords'][idx]
@@ -212,14 +212,13 @@ class Whole_Slide_Bag_FP(Dataset):
 		#storage_client = storage.Client()
 		#bucket = storage_client.bucket("oncomerge")
 		
-        """
-        blob = bucket.blob(self.gs_slide_file_path)
+		"""
+		blob = bucket.blob(self.gs_slide_file_path)
 		with blob.open("rb") as f:
 		  wsi = TiffSlide(f)
 		  img = wsi.read_region(location = (coord[0], coord[1]), level = self.patch_level, size = (self.patch_size, self.patch_size)).convert('RGB')
-        """
-         
-         img = self.wsi .read_region(location = (coord[0], coord[1]), level = self.patch_level, size = (self.patch_size, self.patch_size)).convert('RGB')
+		"""
+		img = self.wsi .read_region(location = (coord[0], coord[1]), level = self.patch_level, size = (self.patch_size, self.patch_size)).convert('RGB')
 		#img = self.wsi.read_region((300, 400), level = 0, size = (512, 512)).convert('RGB')
 
 		#img = self.img
